a68b394a919a7e017f72b7daffa54e2d6310b07a
cuttarug <lpolepeddi@gmail.com>
2013-04-09 21:44:27 -0600

    Sighting model

--- routes.py
+++ routes.py
@@ -6,5 +6,19 @@ db = SQLAlchemy(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://your-username:your-password@localhost/ufosightings'
 
+
+class Sighting(db.Model):
+  __tablename__ = 'sightings'
+  id = db.Column(db.Integer, primary_key = True)
+  sighted_at = db.Column(db.Integer)
+  reported_at = db.Column(db.Integer)
+  location = db.Column(db.String(100))
+  shape = db.Column(db.String(10))
+  duration = db.Column(db.String(10))
+  description = db.Column(db.Text)
+  lat = db.Column(db.Float(6))
+  lng = db.Column(db.Float(6))
+
+
 if __name__ == '__main__':
   app.run(debug=True)
